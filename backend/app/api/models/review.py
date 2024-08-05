7f9d224a05d204499ceaac25b6b21aed7a70d8b4
tonybnya <nya.tony2010@gmail.com>
2024-08-05 12:13:27 +0100

    feat: add a model for the product review

---
+++ backend/app/api/models/review.py
@@ -0,0 +1,21 @@
+"""
+Model for the request body of a review.
+A request body is the data sent by a client to the API.
+"""
+from __future__ import annotations
+
+from datetime import datetime
+
+from pydantic import BaseModel, Field, conint, constr
+
+
+class Review(BaseModel):
+    """Define a model for a product review."""
+    id: int
+    user_id: int
+    product_id: int
+    rating: conint(ge=1, le=5)
+    title: constr(max_length=100)
+    content: str
+    creation_update: datetime = Field(default_factory=datetime.now)
+    update_date: datetime = Field(default_factory=datetime.now)
