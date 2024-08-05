b84805ffbdd73ad89448f61c1df07704f3db4d67
tonybnya <nya.tony2010@gmail.com>
2024-08-05 13:49:48 +0100

    feat: add cart schema

---
+++ backend/app/schemas/cart.py
@@ -0,0 +1,37 @@
+"""
+SQLAlchemy schema for the cart model.
+"""
+
+from datetime import datetime
+
+from db.base import Base
+from sqlalchemy import Column, DateTime, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+
+
+class Cart(Base):
+    """Define the cart schema."""
+
+    __tablename__ = "carts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    creation_date = Column(DateTime, default=datetime.now)
+    update_date = Column(DateTime, default=datetime.now)
+
+    user = relationship("User", back_populates="cart")
+    cart_items = relationship("CartItem", back_populates="cart")
+
+
+class CartItem(Base):
+    """Define the cart item schema."""
+
+    __tablename__ = "cart_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cart_id = Column(Integer, ForeignKey("carts.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+
+    cart = relationship("Cart", back_populates="cart_items")
+    product = relationship("Product", back_populates="cart_items")
